3d7b938b23bc5c67698367b7fae3ddf2903e3051
Percy Woeds <percywoeds@gmail.com>
2017-09-14 20:42:12 +0000

    a

--- app/controllers/notacredits_controller.rb
+++ app/controllers/notacredits_controller.rb
@@ -245,7 +245,8 @@ Banco de CREDITO Cuenta Corriente soles : 191-2231128-0-45 CCI : 002191002231128
                              additional_monetary_totals: [{id: "1001", payable_amount: $lcVVenta}], tax_totals: [{amount: $lcIgv, type: :igv}], legal_monetary_total: $lcTotal}
         
         if $lcMoneda == 2
-              $lcMonedaValor ="USD"
+              puts "dolares "
+                $lcMonedaValor ="USD"
              credit_note = SUNAT::CreditNote.new(credit_note_data)
   
             $aviso = 'Nota enviada con exito...'
