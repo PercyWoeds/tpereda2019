fc14fcc3a381320c57f55129226d1edd28dd21bc
Percy Woeds <percywoeds@gmail.com>
2017-12-12 15:55:17 +0000

    a

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -163,9 +163,9 @@ OPERACION SUJETA AL SISTEMA DE PAGO DE OBLIGACIONES TRIBUTARIAS CON EL GOBIERNO 
             end 
         else
             if $lcMoneda == 2
-                case_52 = ReceiptGenerator.new(8, 52, 4, "BB01").with_igv
+                case_52 = ReceiptGenerator.new(8, 52, 4, "BB01").with_igv(true)
             else        
-                case_96 = ReceiptGenerator.new(12, 96, 3, "BB01").with_different_currency
+                case_96 = ReceiptGenerator.new(12, 96, 3, "BB01").with_different_currency(true)
             end 
         end     
         $lcGuiaRemision =""      
@@ -204,9 +204,9 @@ OPERACION SUJETA AL SISTEMA DE PAGO DE OBLIGACIONES TRIBUTARIAS CON EL GOBIERNO 
             end 
         else
             if $lcMoneda == 2
-                case_52 = ReceiptGenerator.new(8, 52, 4, "BB01").with_igv2
+                case_52 = ReceiptGenerator.new(8, 52, 4, "BB01").with_igv2(true)
             else        
-                case_96 = ReceiptGenerator.new(12, 96, 3, "BB01").with_different_currency2
+                case_96 = ReceiptGenerator.new(12, 96, 3, "BB01").with_different_currency2(true)
             end 
         end 
         
