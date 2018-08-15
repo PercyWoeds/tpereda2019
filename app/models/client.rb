f18a1171a034ffb97614fb23e74bc61f0bd503d6
Percy Woeds <percywoeds@gmail.com>
2018-08-15 17:29:02 +0000

    a

--- app/models/client.rb
+++ app/models/client.rb
@@ -11,7 +11,7 @@ class Client < ActiveRecord::Base
       def self.import(file)
           CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
           
-          a = Client.find_by(row["vruc"])
+          a = Client.find_by(vruc: row["vruc"])
          if a 
          else
           Client.create! row.to_hash 
